f66f41e4fbd3cee1d84a1e598e45676fddd8221d
Jim Kirkbride <james.kirkbride@gmail.com>
2017-06-04 09:23:59 -0400

    Extracting base class CardinalSearch.

---
+++ src/main/kotlin/CardinalSearch.kt
@@ -0,0 +1,32 @@
+open abstract class CardinalSearch(val word: String, val puzzle: Array<Array<String>>) {
+    protected fun searchCardinally(index: Int, word: String, collatedWord: String, reversed: Boolean = false): String? {
+        val directedWord = when {
+            reversed -> word.reversed()
+            else -> word
+        }
+        val getWordIndicesWithDirection = when {
+            reversed -> { foundIndex: Int, word: String -> getWordIndices(foundIndex, word).reversed()}
+            else -> { foundIndex: Int, word: String -> getWordIndices(foundIndex, word) }
+        }
+
+        val foundIndex: Int = collatedWord.indexOf(directedWord)
+        return if (foundIndex > -1) {
+            val wordIndices: IntProgression = getWordIndicesWithDirection(foundIndex, directedWord)
+            buildCoordinateString(wordIndices, index, word)
+        } else {
+            null
+        }
+    }
+
+    private fun getWordIndices(foundIndex: Int, word: String): IntRange {
+        return IntRange(foundIndex, foundIndex + word.length - 1)
+    }
+
+    private fun buildCoordinateString(wordIndices: IntProgression, index: Int, word: String): String {
+        val coordinates: String = buildCoordinates(wordIndices, index).reduce({ accum, coords -> accum + coords.removeSuffix(",")})
+
+        return "$word: $coordinates"
+    }
+
+    protected abstract fun buildCoordinates(wordIndices: IntProgression, index: Int) : List<String>
+}
--- src/main/kotlin/VerticalSearch.kt
+++ src/main/kotlin/VerticalSearch.kt
@@ -1,4 +1,5 @@
-class VerticalSearch(val word: String, val puzzle: Array<Array<String>>) {
+
+class VerticalSearch(word: String, puzzle: Array<Array<String>>) : CardinalSearch(word, puzzle) {
 
     fun execute(): List<String?> {
         val columnIndices: IntRange = IntRange(0, puzzle[0].size - 1)
@@ -9,32 +10,5 @@ class VerticalSearch(val word: String, val puzzle: Array<Array<String>>) {
         }.filterNotNull()
     }
 
-    private fun searchCardinally(index: Int, word: String, collatedWord: String, reversed: Boolean = false): String? {
-        val directedWord = when {
-            reversed -> word.reversed()
-            else -> word
-        }
-        val getWordIndicesWithDirection = when {
-            reversed -> {foundIndex: Int, word: String -> getWordIndices(foundIndex, word).reversed()}
-            else -> { foundIndex: Int, word: String -> getWordIndices(foundIndex, word) }
-        }
-
-        val foundIndex: Int = collatedWord.indexOf(directedWord)
-        return if (foundIndex > -1) {
-            val wordIndices: IntProgression = getWordIndicesWithDirection(foundIndex, directedWord)
-            buildCoordinateString(wordIndices, index, word)
-        } else {
-            null
-        }
-    }
-
-    private fun getWordIndices(foundIndex: Int, word: String): IntRange {
-        return IntRange(foundIndex, foundIndex + word.length - 1)
-    }
-
-    private fun buildCoordinateString(wordIndices: IntProgression, index: Int, word: String): String {
-        val coordinates: String = wordIndices.map { "($index,$it)," }.reduce({ accum, coords -> accum + coords.removeSuffix(",")})
-
-        return "$word: $coordinates"
-    }
+    override fun buildCoordinates(wordIndices: IntProgression, index: Int) = wordIndices.map { "($index,$it)," }
 }
