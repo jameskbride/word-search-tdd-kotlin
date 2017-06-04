ab8742178993e1ac90a80131df948ac40acce398
Jim Kirkbride <james.kirkbride@gmail.com>
2017-06-03 21:41:48 -0400

    Refactoring.

--- src/main/kotlin/WordSearch.kt
+++ src/main/kotlin/WordSearch.kt
@@ -1,21 +1,29 @@
 class WordSearch(val words: List<String>, val puzzle: Array<Array<String>>) {
     fun search(): List<String> {
-        val output: MutableList<String> = mutableListOf()
+        val output: MutableList<String?> = mutableListOf()
         for (word in words) {
             for (rowIndex in puzzle.indices) {
-                val collatedRow: String = puzzle[rowIndex].map { it }.reduce({accum, char -> "$accum$char"})
-                val foundIndex: Int = collatedRow.indexOf(word)
-                if (foundIndex > -1) {
-                    val wordIndices: IntRange = IntRange(foundIndex, foundIndex + word.length -1)
-                    val coordinates: String = wordIndices.map { "($it,$rowIndex)," }.reduce({accum, coords ->
-                        accum + coords.removeSuffix(",")
-                    })
-                    var foundString: String = "$word: $coordinates"
-                    output.add(foundString)
-                }
+                val result: String? = searchHorizontally(rowIndex, word)
+                output.add(result)
             }
         }
 
-        return output
+        return output.filterNotNull()
+    }
+
+    private fun searchHorizontally(rowIndex: Int, word: String): String? {
+        val collatedRow: String = puzzle[rowIndex].map { it }.reduce({ accum, char -> "$accum$char" })
+        val foundIndex: Int = collatedRow.indexOf(word)
+        if (foundIndex > -1) {
+            val wordIndices: IntRange = IntRange(foundIndex, foundIndex + word.length - 1)
+            val coordinates: String = wordIndices.map { "($it,$rowIndex)," }.reduce({ accum, coords ->
+                accum + coords.removeSuffix(",")
+            })
+            var foundString: String = "$word: $coordinates"
+
+            return foundString
+        }
+
+        return null
     }
 }
