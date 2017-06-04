3947a08cbce41229230e4d7562d85877578a0d85
Jim Kirkbride <james.kirkbride@gmail.com>
2017-06-04 07:50:45 -0400

    Refactoring.

--- src/main/kotlin/WordSearch.kt
+++ src/main/kotlin/WordSearch.kt
@@ -5,22 +5,22 @@ class WordSearch(val words: List<String>, val puzzle: Array<Array<String>>) {
         words.forEach { word ->
             output.addAll(puzzle.indices.map { rowIndex ->
                 val collatedRow: String = puzzle[rowIndex].map { it }.reduce({ accum, char -> "$accum$char" })
-                searchHorizontally(rowIndex, word, collatedRow) ?:
-                    searchHorizontally(rowIndex, word, collatedRow, reversed = true)
+                searchCardinally(rowIndex, word, collatedRow, direction = Direction.HORIZONTAL) ?:
+                        searchCardinally(rowIndex, word, collatedRow, direction = Direction.HORIZONTAL, reversed = true)
             })
 
             val columnIndices: IntRange = IntRange(0, puzzle[0].size - 1)
             output.addAll(columnIndices.map { columnIndex ->
                 val collatedColumn: String = puzzle.indices.map { puzzle[it][columnIndex] }.reduce({accum, char -> "$accum$char"})
-                searchVertically(columnIndex, word, collatedColumn) ?:
-                        searchVertically(columnIndex, word, collatedColumn, reversed = true)
+                searchCardinally(columnIndex, word, collatedColumn, direction = Direction.VERTICAL) ?:
+                        searchCardinally(columnIndex, word, collatedColumn, direction = Direction.VERTICAL, reversed = true)
             })
         }
 
         return output.filterNotNull()
     }
 
-    private fun searchVertically(columnIndex: Int, word: String, collatedColumn: String, reversed: Boolean = false): String? {
+    private fun searchCardinally(columnIndex: Int, word: String, collatedColumn: String, reversed: Boolean = false, direction: Direction): String? {
         val directedWord = when { reversed -> word.reversed() else -> word }
         val getWordIndicesWithDirection = when {
             reversed -> {foundIndex: Int, word: String -> getWordIndices(foundIndex, word).reversed()}
@@ -30,23 +30,7 @@ class WordSearch(val words: List<String>, val puzzle: Array<Array<String>>) {
         val foundIndex: Int = collatedColumn.indexOf(directedWord)
         return if (foundIndex > -1) {
             val wordIndices: IntProgression = getWordIndicesWithDirection(foundIndex, directedWord)
-            buildVerticalCoordinateString(wordIndices, columnIndex, word)
-        } else {
-            null
-        }
-    }
-
-    private fun searchHorizontally(rowIndex: Int, word: String, collatedRow: String, reversed: Boolean = false): String? {
-        val directedWord = when { reversed -> word.reversed() else -> word }
-        val getWordIndicesWithDirection = when {
-            reversed -> {foundIndex: Int, word: String -> getWordIndices(foundIndex, word).reversed()}
-            else -> {foundIndex: Int, word: String -> getWordIndices(foundIndex, word)}
-        }
-
-        val foundIndex: Int = collatedRow.indexOf(directedWord)
-        return if (foundIndex > -1) {
-            val wordIndices: IntProgression = getWordIndicesWithDirection(foundIndex, directedWord)
-            buildHorizontalCoordinateString(wordIndices, rowIndex, word)
+            buildCoordinateString(wordIndices, columnIndex, word, direction)
         } else {
             null
         }
@@ -56,19 +40,18 @@ class WordSearch(val words: List<String>, val puzzle: Array<Array<String>>) {
          return IntRange(foundIndex, foundIndex + word.length - 1)
     }
 
-    private fun buildHorizontalCoordinateString(wordIndices: IntProgression, rowIndex: Int, word: String): String {
-        val coordinates: String = wordIndices.map { "($it,$rowIndex)," }.reduce({ accum, coords ->
-            accum + coords.removeSuffix(",")
-        })
+    private fun buildCoordinateString(wordIndices: IntProgression, rowIndex: Int, word: String, direction: Direction): String {
+        val coordinateMapper = when(direction) {
+            Direction.HORIZONTAL -> wordIndices.map { "($it,$rowIndex)," }
+            Direction.VERTICAL -> wordIndices.map { "($rowIndex,$it)," }
+        }
+        val coordinates: String = coordinateMapper.reduce({ accum, coords ->
+        accum + coords.removeSuffix(",")})
 
         return "$word: $coordinates"
     }
 
-    private fun buildVerticalCoordinateString(wordIndices: IntProgression, index: Int, word: String): String {
-        val coordinates: String = wordIndices.map { "($index,$it)," }.reduce({ accum, coords ->
-            accum + coords.removeSuffix(",")
-        })
-
-        return "$word: $coordinates"
+    private enum class Direction {
+        HORIZONTAL, VERTICAL
     }
 }
