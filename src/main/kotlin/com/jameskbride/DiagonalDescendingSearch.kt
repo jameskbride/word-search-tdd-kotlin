f542fe956ebb2a62da970abaffd38028be491510
Jim Kirkbride <james.kirkbride@gmail.com>
2017-06-10 16:38:47 -0400

    Refactoring.

--- src/main/kotlin/com/jameskbride/DiagonalDescendingSearch.kt
+++ src/main/kotlin/com/jameskbride/DiagonalDescendingSearch.kt
@@ -4,19 +4,25 @@ class DiagonalDescendingSearch(val word: String, val puzzle: Array<Array<String>
 
     fun execute(): List<String?> {
         val matchingVectors: List<String> = buildDiagonalVectors().filter {vector -> vector.indexOf(word) > -1}
+        val coordinates: List<String?> = buildCoordinates(matchingVectors)
+
+        return buildCoordinateString(coordinates)
+    }
+
+    private fun buildCoordinateString(coordinates: List<String?>) = coordinates.map { coords -> "$word: $coords" }
+
+    private fun buildCoordinates(matchingVectors: List<String>): List<String?> {
         val coordinates: List<String?> = matchingVectors.map { vector ->
             val startingRow = vector.indexOf(word)
             val rowRange: IntRange = IntRange(startingRow, startingRow + word.indices.last)
             var currentColumnIndex = rowRange.first
-            rowRange.map { rowIndex -> "($rowIndex,${currentColumnIndex++})"}.joinToString(",")
+            rowRange.map { rowIndex -> "($rowIndex,${currentColumnIndex++})" }.joinToString(",")
         }
-
-        return coordinates.map { coords -> "$word: $coords" }
+        return coordinates
     }
 
     private fun buildDiagonalVectors(): MutableList<String> {
         var collatedVectors: MutableList<String> = mutableListOf()
-
         val rowRange: IntRange = IntRange(0, puzzle.indices.last)
         collatedVectors.addAll(rowRange.map { rowIndex ->
             var currentRowIndex = rowIndex
