60dce82e547072b4f28d7464b129d934f300e070
Jim Kirkbride <james.kirkbride@gmail.com>
2017-06-17 22:01:59 -0400

    Now searching diagonally for ascending words.

--- src/main/kotlin/com/jameskbride/WordSearch.kt
+++ src/main/kotlin/com/jameskbride/WordSearch.kt
@@ -2,6 +2,7 @@ package com.jameskbride
 
 import com.jameskbride.search.cardinal.HorizontalSearch
 import com.jameskbride.search.cardinal.VerticalSearch
+import com.jameskbride.search.diagonal.DiagonalAscendingSearch
 import com.jameskbride.search.diagonal.DiagonalDescendingSearch
 
 class WordSearch(val words: List<String>, val puzzle: Array<Array<String>>) {
@@ -11,6 +12,7 @@ class WordSearch(val words: List<String>, val puzzle: Array<Array<String>>) {
         output.addAll(words.flatMap { word -> HorizontalSearch(word, puzzle).execute() })
         output.addAll(words.flatMap { word -> VerticalSearch(word, puzzle).execute() })
         output.addAll(words.flatMap { word -> DiagonalDescendingSearch(word, puzzle).execute() })
+        output.addAll(words.flatMap { word -> DiagonalAscendingSearch(word, puzzle).execute() })
 
         return output.filterNotNull()
     }
